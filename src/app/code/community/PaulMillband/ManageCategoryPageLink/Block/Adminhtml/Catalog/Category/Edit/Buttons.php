70718221bcab53bb5077950c39b6b28a98f8292f
Paul Millband <millband_paul@hotmail.co.uk>
2016-05-25 21:17:14 +0100

    works ... boom

--- src/app/code/community/PaulMillband/ManageCategoryPageLink/Block/Adminhtml/Catalog/Category/Edit/Buttons.php
+++ src/app/code/community/PaulMillband/ManageCategoryPageLink/Block/Adminhtml/Catalog/Category/Edit/Buttons.php
@@ -3,12 +3,23 @@ class PaulMillband_ManageCategoryPageLink_Block_Adminhtml_Catalog_Category_Edit_
     extends Mage_Adminhtml_Block_Catalog_Category_Abstract {
     public function addButtons()
     {
-        $this->getParentBlock()->getChild('form')
-                    ->addAdditionalButton('some_button_code_here', array(
-                        'label' => $this->helper('[module]')->__('Button label here'),
-                        'class' => 'add',
-                        'onclick' => 'alert(\'It works\')' //change the action of the button to what you need
-        ));
+        $moduleName = 'managecategorypagelink';
+        $buttonLabel = 'Go to category page';
+        $buttonCode = 'PaulMillband_ManageCategoryPageLink_button';
+        
+        $category = $this->getCategory();
+        $destinationUrl = $category->getUrl();
+        $isActive = $category->getIsActive();
+        
+        // only show button for active categories and not add new category page
+        if($isActive){
+            $this->getParentBlock()->getChild('form')
+                        ->addAdditionalButton($buttonCode, array(
+                            'label' => $this->helper($moduleName)->__($buttonLabel),
+                            'class' => 'add',
+                            'onclick' => 'setLocation(\'' . $destinationUrl .'\')'
+            ));
+        }
         return $this;
     }
 }
